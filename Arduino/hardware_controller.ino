db3c51f8053d92b512f2d907908814ebc3fb7dcb
Taylor <taylorrosenblum@gmail.com>
2024-03-17 21:31:39 -0400

    correct hardware controller file

--- Arduino/hardware_controller.ino
+++ Arduino/hardware_controller.ino
@@ -2,18 +2,24 @@ const int piezo0 = A0; // Analog pin connected to the piezo sensor
 const int piezo1 = A1; // Analog pin connected to the piezo sensor
 const int piezo2 = A2; // Analog pin connected to the piezo sensor
 
-int val0 = 0;
-int val1 = 0;
-int val2 = 0;
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Initialize serial communication
 }
+
 void loop() {
-    val0 = analogRead(piezo0);
-    val1 = analogRead(piezo1);
-    val2 = analogRead(piezo2);
-    Serial.print("[");
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    if (command == "REQUEST") {
+      sendDataToPython(); // Send data to Python
+    }
+  }
+}
+
+void sendDataToPython() {
+  // Generate some sample data
+    int val0 = analogRead(piezo0);
+    int val1 = analogRead(piezo1);
+    int val2 = analogRead(piezo2);
     Serial.print(val0);
     Serial.print(",");
     Serial.print(val1);
